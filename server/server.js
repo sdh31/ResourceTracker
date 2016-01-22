e128f4c4af84ed5eaa6c2a6ceb3ef344aae1242a
raswaminathan <rahul.swaminathan@duke.edu>
2016-01-22 00:06:42 +0000

    render basic html

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+var __dirname = '../client';
+
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('default.html');
 });
 
 app.listen(80, function () {
