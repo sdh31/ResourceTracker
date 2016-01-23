186caaba6955f35196ad7e3909f8086e3373415d
raswaminathan <rahul.swaminathan@duke.edu>
2016-01-23 01:01:44 +0000

    created db modules and server initializes tables

---
+++ server/database_modules.js
@@ -0,0 +1,37 @@
+var mysql = require('mysql');
+var fs = require('fs');
+var readline = require('readline');
+
+var pool  = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'db',
+  database: 'test_db'
+});
+
+exports.initializeDB = function(callback) {
+  pool.getConnection(function(err, connection) {
+    if(err) { 
+	console.log(err); 
+	callback(true); 
+	return; 
+    }
+
+var rl = readline.createInterface({
+  input: fs.createReadStream('./create_tables.sql'),
+  terminal: false
+ });
+
+rl.on('line', function(chunk){
+    connection.query(chunk.toString('ascii'), [], function(err, results){
+     
+     if(err) console.log(err);
+    });
+});
+
+rl.on('close', function(){
+  connection.release();
+});
+
+  });
+};
--- server/server.js
+++ server/server.js
@@ -1,48 +1,22 @@
 var express = require('express');
 var app = express();
 var __dirname = '../client';
-var mysql = require('mysql');
 
 var views = require('./routes/views');
 var user_routes = require('./routes/users');
 var resource_routes = require('./routes/resources');
 var reservation_routes = require('./routes/reservations');
+var db_modules = require('./database_modules');
 
 app.use('/', views);
 app.use('/user', user_routes);
 app.use('/resource', resource_routes);
 app.use('/reservations', reservation_routes);
-// First you need to create a connection to the db
-var con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'db',
-  database: 'test_db'
-});
-
-con.connect(function(err){
-  if(err){
-    console.log('Error connecting to Db');
-    return;
-  }
-  console.log('Connection established');
-});
-
-con.query('SELECT * FROM testing',function(err,rows){
-  if(err) throw err;
-
-  console.log('Data received from Db:\n');
-  console.log(rows);
-});
-
-con.end(function(err) {
-  // The connection is terminated gracefully
-  // Ensures all previously enqueued queries are still
-  // before sending a COM_QUIT packet to the MySQL server.
-});
 
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
+
+db_modules.initializeDB(null);
 
 app.listen(80, function () {
   console.log('Example app listening on port 80!');
