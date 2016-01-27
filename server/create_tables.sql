200d8b14152b83072b015806e3be7771b0ad0f19
Chris Dee <deechristopher0@gmail.com>
2016-01-27 05:37:34 +0000

    added changed_by field to resource

--- server/create_tables.sql
+++ server/create_tables.sql
@@ -1,6 +1,6 @@
 USE test_db;
 CREATE TABLE IF NOT EXISTS user (user_id INT NOT NULL AUTO_INCREMENT, username varchar(255) NOT NULL UNIQUE, password varchar(255) NOT NULL, permission_level ENUM('admin', 'user'),PRIMARY KEY (user_id));
-CREATE TABLE IF NOT EXISTS resource (resource_id INT NOT NULL AUTO_INCREMENT, name varchar(255), description varchar(255), max_users INT,PRIMARY KEY (resource_id));
+CREATE TABLE IF NOT EXISTS resource (resource_id INT NOT NULL AUTO_INCREMENT, name varchar(255), description varchar(255), max_users INT, created_by varchar(255) , PRIMARY KEY (resource_id));
 CREATE TABLE IF NOT EXISTS reservation (reservation_id INT NOT NULL AUTO_INCREMENT,start_time datetime, end_time datetime, resource_id INT,PRIMARY KEY (reservation_id), FOREIGN KEY (resource_id) REFERENCES resource(resource_id));
 CREATE TABLE IF NOT EXISTS tag (tag_id INT NOT NULL, tag_name varchar(255), PRIMARY KEY (tag_id));
 CREATE TABLE IF NOT EXISTS user_reservation (user_id INT, reservation_id INT, PRIMARY KEY (user_id, reservation_id), FOREIGN KEY (user_id) REFERENCES user(user_id), FOREIGN KEY (reservation_id) REFERENCES reservation(reservation_id));
