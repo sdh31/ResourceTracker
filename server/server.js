ca07d1bf4cb45c3afdeeb4325e021849f37e9c7c
Stephen Daniel Hughes <stephenhughes@wl-10-190-88-173.wireless.duke.edu>
2016-01-29 10:38:15 -0500

    dropdown
    include minify
    
    include minify
    
    include minify
    
    include minify
    
    include minify
    
    more changes
    
    more changes
    
    more changes
    
    more changes
    
    more changes
    
    more changes
    
    more changes
    
    more changes
    
    more changes
    
    more changes
    
    more changes
    
    more changes
    
    more changes
    
    more changes
    
    more changes
    
    more changes
    
    more changes
    
    more changes
    
    changes
    
    changes
    
    node modules
    
    node modules
    
    node modules
    
    node modules
    
    node modules
    
    node modules
    
    node modules
    
    node modules
    
    node modules
    
    node modules
    
    node modules
    
    node modules
    
    node modules
    
    node modules
    
    node modules
    
    node modules
    
    more node modules

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ var user_routes = require('./routes/users');
 var resource_routes = require('./routes/resources');
 var reservation_routes = require('./routes/reservations');
 var script_routes = require('./routes/scripts');
+var node_modules_routes = require('./routes/node_modules');
 var style_routes = require('./routes/styles');
 var initialize_tables = require('./services/initialize_tables');
 var user_db_functions = require('./services/users');
@@ -31,6 +32,7 @@ app.use('/user', user_routes);
 app.use('/resource', resource_routes);
 app.use('/reservations', reservation_routes);
 app.use('/scripts', script_routes);
+app.use('/node_modules', node_modules_routes);
 app.use('/styles', style_routes);
 
 app.engine('html', require('ejs').renderFile);
