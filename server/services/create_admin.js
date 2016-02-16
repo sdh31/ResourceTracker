d01a9b26075d41159333b0cd8cf5f6e63cbcb668
Chris Dee <deechristopher0@gmail.com>
2016-02-16 19:41:19 +0000

    crete admin fixed

--- server/services/create_admin.js
+++ server/services/create_admin.js
@@ -1,5 +1,4 @@
 var user_services = require('./users');
-var group_services = require('./groups');
 
 exports.createAdmin = function(err) {
 
@@ -13,39 +12,12 @@ exports.createAdmin = function(err) {
 		last_name: 'admin',
 		email_address: 'admin@admin.com'
 	};
-	var admin_group = {
-		username: 'admin',
-		description: 'admin user private group',
-		user_management_permission: true,
-		resource_management_permission: true,
-		reservation_management_permission: true,
-		is_private: true
-	}
-	var addAdminToGroupCallback = function(result){
-		if(result.error == true){
-			console.log('could not create admin group link')
-		}
-		else{
-			console.log('admin completely created :D')
-		}
-	}
-
-	var createAdminGroupCallback = function(result){
-		if(result.error == true){
-			console.log('admin group created')
-		}
-		else{
-			group_services.add_user_to_group(admin_group, addAdminToGroupCallback);
-		}
-	}
 	
 	var createAdminUserCallback = function (result) {
 		if (result.error == true) {
 			console.log('admin already created');
 		} else {
-			admin_group.group_id = result.insertId;
-			admin_group.username = admin.username
-			group_services.create_group(admin_group, createAdminGroupCallback);
+			console.log('admin successfully created')
 		}
 	};
 
