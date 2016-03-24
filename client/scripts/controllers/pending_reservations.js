2c10fd7ed4feb26f5ce3cdfc7e0c52b651015f06
bitnami <ashwin.kommajesula@gmail.com>
2016-03-24 05:04:00 +0000

    get all incomplete resources

--- client/scripts/controllers/pending_reservations.js
+++ client/scripts/controllers/pending_reservations.js
@@ -1,18 +1,19 @@
 'use strict';
 
 angular.module('resourceTracker')
-    .controller('IncompleteReservationCtrl', function ($scope, $http, $location, resourceService) {
+    .controller('PendingReservationCtrl', function ($scope, $http, $location, resourceService) {
 
     	$scope.initializePage = function() {
     		$scope.allResources = [];
     		$scope.selectedResourcesIDs = [];
-    		$scope.selectedResources = [];
     		$scope.resourcesToDisplay = [];
-            // resource_id to array of reservations
-            $scope.resourceReservationMap = {};
             // resource_id to its resource
-            $scope.resourceMap = {};
-
+            $scope.resourceMap = new Map();
+            $scope.reservations = [];
+            // reservation_id to that reservations resources
+            $scope.reservationToResourceMap = new Map();
+            // reservation_id to the rest of the reservation information
+            $scope.reservationMap = new Map();
     		getAllResources();
     	};
 
@@ -26,24 +27,53 @@ angular.module('resourceTracker')
 		   		});
 	   	};
 
-	   	$scope.printResources = function() {
-	   		$scope.selectedResourcesIDs.forEach(function(resource) {
-	   			var resource = $scope.resourceMap[resource.id];
-	   			console.log(resource);
-	   			$scope.selectedResources.push(resource);
-	   		});
-	   		console.log($scope.selectedResources);
-	   	};
-
         var populateResourcesToDisplay = function(resourceData, resourceArray) {
             resourceData.forEach(function(resource) {
 	            var resourceData = {id: resource.resource_id, label: resource.name};
 	            resourceArray.push(resourceData);
-	            $scope.resourceReservationMap[resourceData.id] = resource.reservations;
-	            $scope.resourceMap[resourceData.id] = resource;
+	            $scope.resourceMap.set(resourceData.id, resource);
             });
         };
-    	
+
+        $scope.getReservationForSelectedResources = function(){
+            var rIDArray = [];
+            $scope.selectedResourcesIDs.forEach(function(resource){
+                rIDArray.push(resource.id);
+            });            
+            var reqBody = {resource_ids: rIDArray};
+        	$http.post('/reservation/getReservationsByResources', reqBody).then(function(response) {
+                $scope.reservations = response.data.results;
+                console.log($scope.reservations);
+                createReservationMaps();
+        	}, function(error){
+        		console.log(error);
+        	});
+        };
+
+        var createReservationMaps = function(){
+            $scope.reservations.forEach(function(res){
+                if(!res.is_confirmed){
+                    var res_id = res.reservation_id;
+                    if($scope.reservationToResourceMap.has(res_id)){            
+                        var resourceArray = $scope.reservationToResourceMap.get(res_id);
+                        resourceArray.push(res.resource_id);
+                        $scope.reservationToResourceMap.set(res_id, resourceArray);
+                    } else{
+                        $scope.reservationToResourceMap.set(res_id, [res.resource_id]);                        
+                    }
+                    if(!$scope.reservationMap.has(res_id)){
+                    var res = {end_time: res.end_time,
+                               description: res.reservation_description,
+                               title: res.reservation_title,
+                               start_time: res.start_time};
+                    $scope.reservationMap.set(res_id, res);
+                    console.log($scope.reservationMap);                    
+                    }
+                    console.log($scope.reservationToResourceMap);
+                }
+            });
+        };
+
     	$scope.initializePage();
 
 	});
