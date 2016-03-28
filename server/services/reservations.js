63e034f86e58ab83b61a3e68bb59eaecd62ec7d1
raswaminathan <rahul.swaminathan@duke.edu>
2016-03-28 19:13:58 +0000

    allow you to remove multiple resources from a reservation

--- server/services/reservations.js
+++ server/services/reservations.js
@@ -103,9 +103,9 @@ function deleteReservationsById(reservations, callback) {
     basic_db_utility.performSingleRowDBOperation(deleteReservationsByIdQuery, callback);
 };
 
-function remove_resource_from_reservation(reservation, user, has_auth, callback){
-    var removeResourceFromReservation = reservation_query_builder.buildQueryForRemoveResourceFromReservation(reservation, user);
-    basic_db_utility.performSingleRowDBOperation(removeResourceFromReservation, callback);
+function remove_resources_from_reservation(reservation, user, has_auth, callback){
+    var removeResourcesFromReservation = reservation_query_builder.buildQueryForRemoveResourcesFromReservation(reservation, user);
+    basic_db_utility.performSingleRowDBOperation(removeResourcesFromReservation, callback);
 }
 
 function getAllReservationsForUser(user, callback) {
@@ -208,7 +208,7 @@ module.exports = {
     deleteReservationsById: deleteReservationsById,
     getAllReservationsForUser: getAllReservationsForUser,
     organizeReservations: organizeReservations,
-    remove_resource_from_reservation:remove_resource_from_reservation,
+    remove_resources_from_reservation:remove_resources_from_reservation,
     filterAllowedOverlappingReservations: filterAllowedOverlappingReservations,
     filterResourcesByPermission: filterResourcesByPermission,
     denyResourceReservation: denyResourceReservation,
